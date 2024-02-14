aa9adf154a297eab375a756d6d4f520433c46897
Sunny Vats <layers_tipsier_0n@icloud.com>
2024-02-14 12:50:08 +0530

    Update SecurityConfiguration csrf and cors disabling method. Remove unused imports from TokenRepository.

--- src/main/java/com/dev/sunny/userservice/configs/SecurityConfiguration.java
+++ src/main/java/com/dev/sunny/userservice/configs/SecurityConfiguration.java
@@ -2,24 +2,18 @@ package com.dev.sunny.userservice.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> {
-            try {
-                requests.anyRequest()
-                        .permitAll()
-                        .and()
-                        .cors().disable()
-                        .csrf().disable();
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        });
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll())
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
--- src/main/java/com/dev/sunny/userservice/repositories/TokenRepository.java
+++ src/main/java/com/dev/sunny/userservice/repositories/TokenRepository.java
@@ -3,9 +3,6 @@ package com.dev.sunny.userservice.repositories;
 import com.dev.sunny.userservice.models.Tokens;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Date;
-import java.util.Optional;
-
 public interface TokenRepository extends JpaRepository<Tokens, Long> {
     boolean existsTokensByTokenValueAndIsDeletedFalse(String token);
 
