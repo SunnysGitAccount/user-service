ec28b256bcec7444c22130abc7df36e7cd4afd92
Sunny Vats <layers_tipsier_0n@icloud.com>
2024-02-16 22:56:54 +0530

    Add JJWT dependency to pom Add JwtUtil class to generate a JWT Token for UserServiceImpl login

---
+++ src/main/java/com/dev/sunny/userservice/util/JwtUtil.java
@@ -0,0 +1,19 @@
+package com.dev.sunny.userservice.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
+public class JwtUtil {
+    private static final String SECRET_KEY = "jKjLWupTh";
+
+    public static String generateToken(String userName) {
+        return Jwts.builder()
+                .setSubject(userName)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 10 * 60 * 60 * 1000))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+}
